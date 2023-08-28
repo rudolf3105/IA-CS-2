489d97e018d4aa426805f95dd1a729e3b262a570
rudolf3105 <78217392+rudolf3105@users.noreply.github.com>
2023-08-28 16:27:00 +0200

    displaying video and checking the status of a trick

--- bmx/src/pages/Main.js
+++ bmx/src/pages/Main.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../App.css";
 
 function Main() {
@@ -28,11 +29,18 @@ function Main() {
       padding: "10px",
       border: "2px solid black",
     },
+    tick: {
+      display: "flex",
+      flexDirection: "row",
+      //padding: "10px",
+      border: "2px solid black",
+      alignItems: "center",
+    },
     listItem: {
       display: "flex",
       flex: 1,
-      marginTop: "10px",
-      border: "2px solid black",
+      padding: "10px",
+      justifyContent: "center",
     },
     profile: {
       display: "flex",
@@ -79,7 +87,39 @@ function Main() {
     },
   };
 
-  const skills = ["skill1", "skill2", "skill3", "skill4"];
+  const initialSkills = [
+    {
+      name: "skill1",
+      tutorialText: "Tutorial text for Skill 1",
+      videoUrl: "https://www.youtube.com/embed/0qcWOTXCrUY",
+      checked: false
+    },
+    {
+      name: "skill2",
+      tutorialText: "Tutorial text for Skill 2",
+      videoUrl: "https://www.youtube.com/embed/0qcWOTXCrUY",
+      checked: false
+    },
+    {
+      name: "skill3",
+      tutorialText: "Tutorial text for Skill 3",
+      videoUrl: "https://www.youtube.com/embed/0qcWOTXCrUY",
+      checked: false
+    },
+  ]
+
+  const [selectedSkill, setSelectedSkill] = useState(null);
+  const handleSkillClick = (index) => {
+    setSelectedSkill(skills[index]);
+  };
+
+  const [skills, setSkills] = useState(initialSkills);
+  const handleSkillCheck = (index) => {
+    const updatedSkills = [...skills];
+    updatedSkills[index].checked = !updatedSkills[index].checked;
+    setSkills(updatedSkills);
+  };
+
 
   return (
     <div className="App">
@@ -87,27 +127,51 @@ function Main() {
         <div style={styles.fullScreenDivStyle}>
           <div style={styles.leftContainer}>
             <div style={styles.webName}></div>
-            <div style={styles.tickList}>
-              <ul style={styles.tickList}>
-                {skills.map((name, index) => (
-                  <li key={index} style={styles.listItem}>
-                    {name}
-                  </li>
-                ))}
-              </ul>
-            </div>
+              <div style={styles.tickList}>
+                <ul style={styles.tickList}>
+                  {skills.map((skill, index) => (
+                    <div style={{
+                      ...styles.tick,
+                      backgroundColor: skills[index].checked == true? 'green' : 'red'
+                    }}>
+                      <div>
+                        <button onClick={() => handleSkillCheck(index)}>
+                          O
+                        </button>
+                      </div>
+                      <li
+                      key={index}
+                      style={styles.listItem}
+                      onClick={() => handleSkillClick(index)}
+                      >
+                      {skill.name}
+                      </li>
+                    </div>
+                  ))}
+                </ul>
+              </div>
             <div style={styles.profile}></div>
           </div>
 
           <div style={styles.rightContainer}>
             <div style={styles.videoContainer}>
-              <div style={styles.Title}>title</div>
-              <div style={styles.mainContainer}>video</div>
+              <div style={styles.Title}>{selectedSkill?.name || "Select a skill"}</div>
+              <div style={styles.mainContainer}>
+                {selectedSkill && (
+                  <iframe
+                    width="60%"
+                    height="100%"
+                    src={selectedSkill.videoUrl}
+                    frameborder="0"
+                    allowfullscreen
+                  ></iframe>
+                )}
+              </div>
             </div>
 
             <div style={styles.tutorialContainer}>
-              <div style={styles.Title}>tutorial</div>
-              <div style={styles.mainContainer}>description</div>
+              <div style={styles.Title}>Tutorial</div>
+              <div style={styles.mainContainer}>{selectedSkill?.tutorialText || ""}</div>
             </div>
           </div>
         </div>
